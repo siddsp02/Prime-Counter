f6bfe82007a26b0eadbae6ca9e1abbd65b865a7e
Sidd <35938702+siddsp02@users.noreply.github.com>
2021-12-17 23:58:12 -0800

    Used logical complement for performance increase

--- primes.py
+++ primes.py
@@ -5,18 +5,18 @@ A number is considered prime if it is greater than or equal to 2,
 and its only factors are 1 and itself.
 """
 
-from math import isqrt
 import cProfile
 import pstats
+from math import isqrt
 
 
 def count_primes(limit: int) -> int:
     """Returns the number of primes from 2 to the limit specified."""
-    multiples = set(range(2, limit + 1, 2))
+    primes = set(range(3, limit + 1, 2))
     for i in range(3, isqrt(limit) + 1, 2):
-        if i not in multiples:
-            multiples.update(range(i << 1, limit + 1, i))
-    return limit - len(multiples)
+        if i in primes:
+            primes.difference_update(range(i ** 2, limit + 1, i))
+    return len(primes) + 1
 
 
 def main(n: int = 100_000) -> None:
